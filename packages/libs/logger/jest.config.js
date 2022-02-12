7baf476823b38fd6f976d70102bb7075f839687b
Tomasz Najbar <najbartomasz@gmail.com>
2022-02-12 21:11:51 +0100

    Package: Logger. Initial content.
    Added:
    - new logger package under packages/libs directory,
    - package.json file with scripts and dependencies,
    - package-lock.json file,
    - TypeScript configuration for both sources and tests,
    - ESLint configuration for both sources and tests,
    - Jest configuration file.

---
+++ packages/libs/logger/jest.config.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line strict
+'use strict';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+module.exports = require('../../../configs/jest/jest.config');
