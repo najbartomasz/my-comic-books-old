aa45a76c47b1c6c84aec66bf7118153702937beb
Tomasz Najbar <najbartomasz@gmail.com>
2022-04-03 16:35:47 +0200

    Added missing exports to Express http server library.

---
+++ packages/libs/express-http-server/src/index.ts
@@ -0,0 +1,5 @@
+export type { NextFunction, Request, RequestHandler, Response } from 'express';
+export type { OpenApiValidatorOpts } from 'express-openapi-validator/dist/openapi.validator';
+export type { Server } from 'http';
+
+export { createHttpServer } from './modules/server';
